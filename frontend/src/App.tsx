29af357acfffedf56326147c1eb7d01dee9693fa
sadie21393 <sadie21@byu.edu>
2025-03-27 18:31:00 -0600

    category CategoryFilter

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import BookList from "./components/BookList"; // Import the BookList component
+import CategoryFilter from "./components/CategoryFilter";
 
 function App() {
   return (
     <div className="App">
       <h1>Online Bookstore</h1>
+      <CategoryFilter />
       <BookList /> {/* Display the book list */}
     </div>
   );
---
+++ frontend/src/components/CategoryFilter.tsx
@@ -0,0 +1,12 @@
+function CategoryFilter() {
+  return (
+    <>
+      <h5>Filter</h5>
+      <div>
+        <input className="category-checkbox" type="checkbox"></input>
+      </div>
+    </>
+  );
+}
+
+export default CategoryFilter;
