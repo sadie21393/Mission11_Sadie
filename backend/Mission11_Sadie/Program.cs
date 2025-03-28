338658e11029d7486f6d798670eba9b7f6401991
sadie21393 <sadie21@byu.edu>
2025-03-27 18:14:42 -0600

    port changes

--- backend/Mission11_Sadie/Program.cs
+++ backend/Mission11_Sadie/Program.cs
@@ -16,7 +16,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy(name: MyAllowSpecificOrigins,
         policy =>
         {
-            policy.WithOrigins("http://localhost:5174") // React frontend URL
+            policy.WithOrigins("http://localhost:5173") // React frontend URL
                   .AllowAnyMethod()
                   .AllowAnyHeader();
         });
--- backend/Mission11_Sadie/obj/Debug/net8.0/Mission11_Sadie.AssemblyInfo.cs
+++ backend/Mission11_Sadie/obj/Debug/net8.0/Mission11_Sadie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mission11_Sadie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76f43b197c128233e76f9b56341d37cd18e1b694")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mission11_Sadie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mission11_Sadie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
